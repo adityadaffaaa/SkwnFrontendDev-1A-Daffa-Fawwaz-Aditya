cb88a98a543c024fefc8e23af6c29b5292119652
adityadaffaaa <adityadaffafawwaz@gmail.com>
2023-03-08 11:27:35 +0700

    Landing Page

---
+++ js/swiperPartneredBrand.js
@@ -0,0 +1,10 @@
+const swiperPartneredBrand = new Swiper(".partneredBrandsSwiper", {
+  slidesPerView: 4,
+  spaceBetween: 0,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  freeMode: true,
+});
